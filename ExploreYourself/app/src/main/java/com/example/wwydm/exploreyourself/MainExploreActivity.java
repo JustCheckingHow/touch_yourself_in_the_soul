70312d97d36e50c98f9dfadd7b4efd11083f3c3f
aweas <wwydmanski@gmail.com>
2018-05-26 11:37:26 +0200

    Initial app commit

---
+++ ExploreYourself/app/src/main/java/com/example/wwydm/exploreyourself/MainExploreActivity.java
@@ -0,0 +1,13 @@
+package com.example.wwydm.exploreyourself;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainExploreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_explore);
+    }
+}
