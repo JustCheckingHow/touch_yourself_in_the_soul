e15c06e760d2c39dde395f1ff6ddf073bb655cc9
aweas <wwydmanski@gmail.com>
2018-05-26 19:24:24 +0200

    Added command for cleaning db

--- Serwer/dBinterface.py
+++ Serwer/dBinterface.py
@@ -81,7 +81,13 @@ class Unpickler:
                     conn.commit()
         conn.close()
 
+    def clearOrphanedExhibits(self):
+        conn = connect(_DATABASE_)
+        conn.execute("delete from Objects where id not in ( select distinct objectId from subjects)")
+        conn.commit()
+
 u = Unpickler()
 u.loadObjectsTable()
 # u.loadTypesTable()
 # u.loadSubjectsTable()
+u.clearOrphanedExhibits()
