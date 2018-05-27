332d917c928e0a66c52ad6118ef1f23ac91da83e
Jakub <jakub.mojsiejuk@gmail.com>
2018-05-27 17:18:24 +0200

    adding mising files

---
+++ ExploreYourself/app/src/main/java/com/example/wwydm/exploreyourself/NavigationUtility.java
@@ -0,0 +1,17 @@
+package com.example.wwydm.exploreyourself;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jakub on 27/05/2018.
+ */
+
+public class NavigationUtility extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+    }
+}
